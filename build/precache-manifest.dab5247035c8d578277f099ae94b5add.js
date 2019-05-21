c99dfec0b3eb717fc0b3fe21f436f41364560ce9
SAN Vuthy <san.vuthy08@gmail.com>
2019-05-21 16:01:40 +0700

    update

---
+++ build/precache-manifest.dab5247035c8d578277f099ae94b5add.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee4b85fef49ccc1ce24a",
+    "url": "/static/css/main.bfc12597.chunk.css"
+  },
+  {
+    "revision": "ee4b85fef49ccc1ce24a",
+    "url": "/static/js/main.ee4b85fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "4f7fa6aba40d1323385d",
+    "url": "/static/js/2.4f7fa6ab.chunk.js"
+  },
+  {
+    "revision": "a633d884a48ebcc3b0b774fc93674e03",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
-  "/precache-manifest.bd7d049e378a95535c637954e1c93c21.js"
-=======
-  "/precache-manifest.e851d09ba54a935e524d59e087769b3c.js"
->>>>>>> efc58845ff714984aa9e23eb06ddbdf79230dd42
+  "/precache-manifest.dab5247035c8d578277f099ae94b5add.js"
 );
 
 workbox.clientsClaim();
